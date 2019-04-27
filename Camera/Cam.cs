92e88a29e2864ef5405cd1ebdad4423c935733e7
Pavan <44775810+algorithm-planet@users.noreply.github.com>
2019-04-27 19:57:40 +0530

    Add files via upload

---
+++ Camera/Cam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class Cam_path
+{
+    public static Orientation position(Vector3 O , Vector3 K , float yaw , float R = 10f , float t = 0f)
+    {
+        Vector3 v = -Vector3.Cross(K, Vector3.one);
+
+        Vector3 k = -Vector3.Cross(v, K);
+
+        Vector3 n = Tool_.Rotate(v, k, yaw * Mathf.Deg2Rad);
+        float angle = t * 360f;
+        Vector3 new_v = Tool_.Rotate(n, K, angle * Mathf.Deg2Rad);
+
+        return new Orientation(O, new_v * R);
+    }
+}
+
+
+public class Orientation
+{
+    public Vector3 postion;
+    public Vector3 dir;
+
+    public Orientation(Vector3 O , Vector3 v)
+    {
+        postion = O + v;
+        dir = (-v).normalized;
+    }
+}
---
+++ Draw/Draw.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region Draw
+public class Draw
+{
+    public static float time = Time.deltaTime;
+
+    #region bezier
+    public static void bezier(Bezier bezier, Color color, int N = 20)
+    {
+        float t_s = 1f / N;
+        for (float t_i = t_s; t_i <= 1f; t_i = round_decimal(t_i + t_s, 2))
+        {
+            Vector3 current_p = bezier.position(t_i);
+            Vector3 prev_p = bezier.position(t_i - t_s);
+
+            Debug.DrawLine(current_p, prev_p, color, time);
+        }
+    }
+    #endregion
+
+    static float round_decimal(float Input_, int i = 2)
+    {
+        return Mathf.RoundToInt(Input_ * Mathf.Pow(10, i)) / Mathf.Pow(10, i);
+    }
+}
+
+#endregion
